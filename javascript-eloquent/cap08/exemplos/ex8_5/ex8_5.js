148d40cd1c27f528fc28ea3e96a25ae3f0311f34
Michael Wellington Lopes <michael23.wellington@gmail.com>
2023-03-10 11:19:04 -0300

    JavaScript Eloquent - capitulo 08 - parte II (10/03/2023)

---
+++ javascript-eloquent/cap08/exemplos/ex8_5/ex8_5.js
@@ -0,0 +1,6 @@
+function promptNumber(question) {
+  var result = Number(prompt(question, ''));
+  if (isNaN(result)) return null;
+  else return result;
+}
+console.log(promptNumber('How many trees do you see?'));
