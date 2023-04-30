7305132e554b72c9778908119eb113ec04811f42
Michael Wellington Lopes <michael23.wellington@gmail.com>
2023-04-30 16:33:13 -0300

    JavaScript Eloquent - capitulo 21 - Website de comportamento de habilidade + exericios (30/04/2023)

---
+++ javascript-eloquent/cap21/exercicios/exe21_1.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+
+var talks = loadTalks();
+
+function loadTalks() {
+  var result = Object.create(null),
+    json;
+  try {
+    json = JSON.parse(fs.readFileSync('./talks.json', 'utf8'));
+  } catch (e) {
+    json = {};
+  }
+  for (var title in json) result[title] = json[title];
+  return result;
+}
+
+function registerChange(title) {
+  changes.push({ title: title, time: Date.now() });
+  waiting.forEach(function (waiter) {
+    sendTalks(getChangedTalks(waiter.since), waiter.response);
+  });
+  waiting = [];
+
+  fs.writeFile('./talks.json', JSON.stringify(talks));
+}
---
+++ javascript-eloquent/cap21/exercicios/exe21_2.js
@@ -0,0 +1,25 @@
+function displayTalks(talks) {
+  talks.forEach(function (talk) {
+    var shown = shownTalks[talk.title];
+    if (talk.deleted) {
+      if (shown) {
+        talkDiv.removeChild(shown);
+        delete shownTalks[talk.title];
+      }
+    } else {
+      var node = drawTalk(talk);
+      if (shown) {
+        var textField = shown.querySelector('input');
+        var hasFocus = document.activeElement == textField;
+        var value = textField.value;
+        talkDiv.replaceChild(node, shown);
+        var newTextField = node.querySelector('input');
+        newTextField.value = value;
+        if (hasFocus) newTextField.focus();
+      } else {
+        talkDiv.appendChild(node);
+      }
+      shownTalks[talk.title] = node;
+    }
+  });
+}
